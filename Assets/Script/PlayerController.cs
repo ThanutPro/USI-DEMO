db740d1575df4e0e06923876477ec9f4a8d645a2
ThanutPro <Thanutpro@gmail.com>
2024-10-10 02:36:21 +0700

    Added Vertical Movement

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float moveSpeed = 10;
     public float hInput;
+    public float vInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get horizontal and vertical inputs
         hInput = Input.GetAxisRaw("Horizontal");
+        vInput = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(Vector2.right * hInput * moveSpeed * Time.deltaTime);  
+        // Apply horizontal and vertical movement
+        transform.Translate(new Vector2(hInput, vInput) * moveSpeed * Time.deltaTime);
     }
 }
