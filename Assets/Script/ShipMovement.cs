38d6c8dc26dff18a6266a4f0eda56384109355a4
ThanutPro <Thanutpro@gmail.com>
2024-10-11 02:37:46 +0700

    Added Enemy respawn

--- Assets/Script/ShipMovement.cs
+++ Assets/Script/ShipMovement.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class ShipMovement : MonoBehaviour
 {
     [SerializeField]
-    public float moveSpeed = 2;
+    public float moveSpeed = 2f;
     private Vector3 startPosition; // Variable to store the starting position
     private int loopCounter = 0; // Counter to track the number of loops
-    private int maxLoops = 8; // Set the number of loops before resetting to start position
+    private int maxLoops = 4; // Set the number of loops before respawning
+
+    // Reference to the Enemy Ships prefab
+    public GameObject enemyShipsPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         // Save the initial starting position of the ship
         startPosition = transform.position;
+
+        // Optionally, you could spawn the enemy ships here if not already in the scene
+        // SpawnEnemyShips();
     }
 
     // Update is called once per frame
@@ -27,7 +33,7 @@ public class ShipMovement : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Check if the ship hits a boundary and reverse direction
-        if (collision.gameObject.tag == "Boundary")
+        if (collision.gameObject.CompareTag("Boundary"))
         {
             transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
             moveSpeed *= -1;
@@ -40,9 +46,28 @@ public class ShipMovement : MonoBehaviour
             {
                 // Reset the ship to its starting position
                 transform.position = startPosition;
+
                 // Reset the loop counter
                 loopCounter = 0;
+
+                // Respawn the enemy ships
+                RespawnEnemyShips();
             }
         }
+    }
+
+    private void RespawnEnemyShips()
+    {
+        // Destroy all existing enemy ships
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Instantiate a new set of enemy ships
+        GameObject newEnemyShips = Instantiate(enemyShipsPrefab, startPosition, Quaternion.identity);
+
+        // Optionally, you can set the parent of the new enemy ships to this object
+        newEnemyShips.transform.parent = transform;
     }
 }
